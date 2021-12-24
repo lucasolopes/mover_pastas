d2bfa48242bcbc54563dfe9b6541bf9db228f812
Lucas Olopes <62860227+OlopesMaster@users.noreply.github.com>
2021-12-24 01:14:58 -0300

    Add files via upload

---
+++ mover.py
@@ -0,0 +1,17 @@
+import os
+import pathlib
+
+def mover_arquivos(caminho_origem, caminho_destino, formato):
+    lista = list(set(str(path.parent) for path in pathlib.Path(f"{caminho_origem}\\.").glob(f"**/*{formato}")))
+    nome_arquivo = list()
+    aux = 0
+    for c in lista:
+        nome_arquivo.append(pathlib.PureWindowsPath(str(lista[aux])).name)
+        aux+=1
+
+    print(nome_arquivo)
+    print(lista)
+    aux = 0
+    for arquivo in lista: 
+        os.rename(f"{arquivo}", f"{caminho_destino}\\{nome_arquivo[aux]}")
+        aux+=1
